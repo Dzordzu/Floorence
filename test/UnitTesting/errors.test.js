e96d75732c823d8ce428a1698aed5768f5ba2065
Dzordzu <tomekdur@wp.pl>
2018-09-14 14:41:47 +0200

    Upgarded tests

--- test/UnitTesting/errors.test.js
+++ test/UnitTesting/errors.test.js
@@ -1,8 +1,28 @@
 
-import { FloorenceError } from '../../src/Errors/FloorenceError.js';
+import FloorenceError from './../../src/Errors/FloorenceError.js';
+import { assert } from 'chai'
 
-describe('Errors', () => {
-  describe('#FloorenceError', () => {
-    
+describe('Errors', function() {
+  describe('#FloorenceError', function() {
+    it('Should catch Floorence Errors', function() {
+
+      let number = 10;
+      let message = 'This is test message';
+      let userMessage = 'Another one';
+
+      try {
+        throw new FloorenceError(message, {
+          number: 10,
+          userMessage: userMessage
+        });
+      } catch(error) {
+        assert.strictEqual(error.name, 'FloorenceError', 'Wrong error type');
+        assert.strictEqual(error.message, message, 'Messages are not equal');
+        assert.strictEqual(error.userMessage, userMessage, 'User Messages are not equal');
+        assert.strictEqual(error.number, number, 'Numbers are not equal');
+        assert.strictEqual(error.getUserMessage(), userMessage, 'Numbers are not equal');
+      }
+    });
+
   });
 });
