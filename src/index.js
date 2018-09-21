0f2ddb571355678fbf284787064c65846db3014d
Dzordzu <tomekdur@wp.pl>
2018-09-22 00:13:47 +0200

    Added lodash, started creating main facade (Floorence)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 
+import { defaultsDeep } from 'lodash'
+
 class Floorence {
 
-  constructor() {
+  constructor(options = {}) {
+
+    this.options = defaultsDeep(options, defaults.Floorence);
 
     /**
      * Barba.js style
@@ -11,11 +15,43 @@ class Floorence {
     this.Cache = Cache;
     this.Dispatcher = Dispatcher;
     this.StateManager = StateManager;
-
-    /**
-     * Pjax style
-     */
   }
+
+  /**
+   * Checks if browser supports Pjax
+   */
+  supports() {}
+  /**
+   *  Aliases for floorence.Pjax.start() and floorence.Pjax.end()
+   */
+  start() {}
+  stop() {}
+  /**
+   * Force Pjax to load URL
+   * It also shows loader using floorence.transition.show method
+   */
+  loadUrl() {}
+  /**
+   * [handleResponse description]
+   * @return {[type]} [description]
+   */
+  handleResponse() {}
+  /**
+   * Apply Floorence for the loaded elements
+   * Internally it just use stop() and then start()
+   */
+  refresh() {}
+  /**
+   * Perform some tasks AFTER handleResponse() is called
+   */
+  onLoad() {}
+  /**
+   * Stop pjax
+   * It also closes loader using floorence.transition.close method
+   */
+  cancel() {}
+
+
 
 }
 module.export = Floorence;
