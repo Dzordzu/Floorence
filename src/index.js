718cc4e0265a1cbb055af133b32bc91569fa6563
Dzordzu <tomekdur@wp.pl>
2018-09-21 23:27:22 +0200

    Removed unnecessary comments

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 
+class Floorence {
+
+  constructor() {
+
+    /**
+     * Barba.js style
+     */
+    this.Pjax = Pjax;
+    this.Transition = Transition;
+    this.Cache = Cache;
+    this.Dispatcher = Dispatcher;
+    this.StateManager = StateManager;
+
+    /**
+     * Pjax style
+     */
+  }
+
+}
+module.export = Floorence;
--- tests/ManualTesting/history.test.js
+++ tests/ManualTesting/history.test.js
@@ -21,16 +21,10 @@ describe('HistoryManager', function() {
       let display = url.expected ? url.expected.display : (url.display || url.real);
 
       it(`loadUrl() #${index}`, function() {
-        
         chai.expect(historyManager.loadUrl(url), 'Returned value is incorrect').to.be.true;
         chai.expect(window.history.state, 'History state didn\'t change').to.deep.equal(url);
         chai.expect(window.location.pathname, 'Window location didn\'t changed').to.equal(display);
 
-        // TDD style
-        // chai.assert.deepEqual(historyManager.loadUrl(url), true, 'Returned value is incorrect');
-        // chai.assert.deepEqual(window.history.state, url, 'History state didn\'t change');
-        // chai.assert.strictEqual(window.location.pathname, display, 'Window location didn\'t changed');
-        //
       });
     });
 
